2e4a326a0e8ac215e1fcb8015be97bd58c0148dc
abrahamrodmi <carlos.arodmi31@gmail.com>
2026-05-02 22:57:37 -0600

    otro rollo

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,19 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { Role } from '../../enums/roles.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) { }
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @ApiProperty({ example: 'Don Usuario XD', required: false })
+    nombre?: string;
+
+    @ApiProperty({ example: 'don_usuario_xd', required: false })
+    username?: string;
+
+    @ApiProperty({
+        enum: Role,
+        example: Role.USER,
+        required: false,
+        description: 'Roles disponibles: ADMIN, DEVELOPER, USER'
+    })
+    rol?: Role; // <-- Usa el Enum aquí en lugar de string
+}
